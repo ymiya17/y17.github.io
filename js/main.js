89c04de66c6aea0a9f8ac22d0f78595309e8aa15
ymiya17 <78059407+ymiya17@users.noreply.github.com>
2021-03-08 17:33:11 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,61 @@
+'use strict';
+
+console.clear();
+
+{
+  const year = 2020;
+  const month = 4; // 5月
+
+  function getCalendarHead() {
+    const dates = [];
+    const d = new Date(year, month, 0).getDate();
+    const n = new Date(year, month, 1).getDay();
+
+    for (let i = 0; i < n; i++) {
+      // 30
+      // 29, 30
+      // 28, 29, 30
+      dates.unshift({
+        date: d - i,
+        isToday: false,
+        isDisabled: true,
+      });
+    }
+
+    console.log(dates);
+  }
+
+  function getCalendarBody() {
+    const dates = []; // date: 日付, day: 曜日
+    const lastDate = new Date(year, month + 1, 0).getDate();
+
+    for (let i = 1; i <= lastDate; i++) {
+      dates.push({
+        date: i,
+        isToday: false,
+        isDisabled: false,
+      });
+    }
+
+    console.log(dates);
+  }
+
+  function getCalendarTail() {
+    const dates = [];
+    const lastDay = new Date(year, month + 1, 0).getDay();
+
+    for (let i = 1; i < 7 - lastDay; i++) {
+      dates.push({
+        date: i,
+        isToday: false,
+        isDisabled: true,
+      });
+    }
+
+    console.log(dates);
+  }
+
+  // getCalendarHead();
+  // getCalendarBody();
+  getCalendarTail();
+}
