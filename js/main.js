69402d177b32b27d003c50d89c335a2db73cfeb3
ymiya17 <78059407+ymiya17@users.noreply.github.com>
2021-03-12 16:58:57 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -150,24 +150,7 @@ console.clear();
     const btn = document.getElementById('btn');
 
     btn.addEventListener('click',() =>{
-        // // const results = ['大吉', '中吉', '凶', '末吉'];
-        // const results = ['大吉', '大吉', '大吉', '大吉','凶'];
-        // // const n = Math.floor(Math.random() * results.length);
-        // // btn.textContent = results[n];
-        // btn.textContent = results[Math.floor(Math.random() * results.length)];
-    
-//    switch(n){
-//        case 0:
-//            btn.textContent ='大吉'
-//            break;
-//        case 1:
-//            btn.textContent ='中吉'
-//            break;
-//        case 2:
-//            btn.textContent ='凶'
-//            break;
-
-//    }
+       
    const n = Math.random();
    if (n < 0.05){
        btn.textContent ='大吉';
